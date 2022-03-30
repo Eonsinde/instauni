57086a126fd9994afd8fd48d71f33aa67d914412
Eonsinde <olasinde.eon@gmail.com>
2022-03-31 00:13:56 +0100

    Lazy loaded components using React.lazy

--- frontend/src/layout/BaseView.js
+++ frontend/src/layout/BaseView.js
@@ -1,41 +1,77 @@
 import React from 'react';
-import { Link, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+
+import { AnimatePresence } from 'framer-motion';
 
 import Header from './Header';
-import Objectives from './Objectives';
-import CreateTask from '../forms/CreateTask';
-import Test from './Test';
 
-import Dashboard from './Dashboard/Dashboard';
+// import Dashboard from './Dashboard/Dashboard';
 
 import Login from '../forms/Login';
 import Register from '../forms/Register';
 import PageNotFound from './PageNotFound';
+// import FindTask from './FindTask';
+
+import PageLoader from '../loaders/PageLoader';
+
+const Objectives = React.lazy(() => import ("./Objectives"));
+const Test = React.lazy(() => import("./Test"));
+const CreateTask = React.lazy(() => import("../forms/CreateTask"));
+const FindTask = React.lazy(() => import("./FindTask"));
+const Dashboard = React.lazy(() => import("./Dashboard/Dashboard"));
 
 
 const BaseView = () => {
     return ( 
         <>
             <Header />
-            <Routes>
-                <Route path="/" element={<Test />} />
-                <Route path="home" element={
-                    <>
-                        <Test />
-                        <h3>egtrhggrgrtgrgergeg</h3>
-                    </>
-                } />
-                <Route path="objectives" element={<Objectives />} />
-                <Route path="create-task" element={<CreateTask />} />
+            <AnimatePresence exitBeforeEnter>
+                <Routes>
+                    <Route path="/" element={<>Hello! Welcome to instauni</>} />
+                    <Route path="home" element={
+                        <React.Suspense fallback={<PageLoader />}>
+                            <Test />
+                        </React.Suspense>
+                    } />
+                    <Route path="objectives" 
+                        element={
+                            <React.Suspense fallback={<PageLoader />}>
+                                <Objectives />
+                            </React.Suspense>
+                        } 
+                    />
+                    <Route path="page-loader" element={<PageLoader />} />
+                    <Route path="create-task" 
+                        element={
+                            <React.Suspense fallback={<PageLoader />}>
+                                <CreateTask />
+                            </React.Suspense>
+                        } 
+                    />
+                    <Route path="find-task" 
+                        element={
+                            <React.Suspense fallback={<PageLoader />}>
+                                <FindTask />
+                            </React.Suspense>
+                        } 
+                    />
 
-                {/* Auth aspects */}
-                <Route path='/login' element={<Login />} />
-                <Route path='/register' element={<Register />} />
-                {/* Dashboard/Components to be rendered in dashboard */}
-                <Route path='dashboard/*' element={<Dashboard />} />
-                
-                <Route path='*' element={<PageNotFound />} />
-            </Routes>
+                    {/* Dashboard/Components to be rendered in dashboard */}
+                    <Route path="dashboard/*" 
+                        element={
+                            <React.Suspense fallback={<></>}>
+                                <Dashboard />
+                            </React.Suspense>
+                        } 
+                    />
+
+                    {/* Auth aspects */}
+                    <Route path='/login' element={<Login />} />
+                    <Route path='/register' element={<Register />} />
+                    
+                    <Route path='*' element={<PageNotFound />} />
+                </Routes>
+            </AnimatePresence>
         </>
     );
 }
--- frontend/src/layout/Dashboard/Dashboard.js
+++ frontend/src/layout/Dashboard/Dashboard.js
@@ -1,30 +1,49 @@
 import React from 'react';
-import { Link, Routes, Route } from 'react-router-dom';
-import Profile from './Profile';
-import TaskHistory from './TaskHistory';
-import TasksMonitor from './TasksMonitor';
-import Wallet from './Wallet';
+import { NavLink, Routes, Route } from 'react-router-dom';
+import DashboardLoader from '../../loaders/DashboardLoader';
+
+const Profile = React.lazy(() => import ("./Profile"));
+const TasksMonitor = React.lazy(() => import ("./TasksMonitor"));
+const TaskHistory = React.lazy(() => import ("./TaskHistory"));
+const Wallet = React.lazy(() => import ("./Wallet"));
 
 
 const Dashboard = () => {
     return ( 
         <section className='py-28'>
             <div className='container mx-auto flex flew-row items-center'>
-                <aside className='flex justify-center items-center basis-2/5 h-auto'>
+                <aside className='flex justify-center items-center basis-1/5 md:basis-2/5 h-auto'>
                     <ul className='flex flex-col justify-center items-center'>
-                        <li className='mb-10'><Link to='/dashboard/user/profile' className=''>Profile</Link></li>
-                        <li className='mb-10'><Link to='/dashboard/user/wallet' className=''>Wallet</Link></li>
-                        <li className='mb-10'><Link to='/dashboard/user/monitor-tasks' className=''>Monitor</Link></li>
-                        <li className=''><Link to='/dashboard/user/history' className=''>History</Link></li>
+                        <li className='mb-10'><NavLink className={({isActive}) => isActive ? 'font-semibold' : undefined} to='/dashboard/user/2/profile'>Profile</NavLink></li>
+                        <li className='mb-10'><NavLink className={({isActive}) => isActive ? 'font-semibold' : undefined} to='/dashboard/user/2/wallet'>Wallet</NavLink></li>
+                        <li className='mb-10'><NavLink className={({isActive}) => isActive ? 'font-semibold' : undefined} to='/dashboard/user/2/monitor-tasks'>Monitor</NavLink></li>
+                        <li className=''><NavLink className={({isActive}) => isActive ? 'font-semibold' : undefined} to='/dashboard/user/2/history'>History</NavLink></li>
                     </ul>
                 </aside>
-                <main className='basis-3/5 h-auto'>
+                <main className='basis-4/5 md:basis-3/5 h-auto'>
                     <Routes>
                         {/* <Route path="user/:id/profile" element={<Profile />} /> */}
-                        <Route path="user/profile" element={<Profile />} />
-                        <Route path="user/wallet" element={<Wallet />} />
-                        <Route path="user/monitor-tasks" element={<TasksMonitor />} />
-                        <Route path="user/history" element={<TaskHistory />} />
+                        <Route path="loader" element={<DashboardLoader />} />
+                        <Route path="user/:id/profile" element={
+                            <React.Suspense fallback={<DashboardLoader />}>
+                                <Profile />
+                            </React.Suspense>
+                        } />
+                        <Route path="user/:id/wallet" element={
+                            <React.Suspense fallback={<DashboardLoader />}>
+                                <Wallet />
+                            </React.Suspense>
+                        } />        
+                        <Route path="user/:id/monitor-tasks" element={
+                            <React.Suspense fallback={<DashboardLoader />}>
+                                <TasksMonitor />
+                            </React.Suspense>
+                        } />    
+                         <Route path="user/:id/history" element={
+                            <React.Suspense fallback={<DashboardLoader />}>
+                                <TaskHistory />
+                            </React.Suspense>
+                        } />    
                     </Routes>
                 </main>
             </div>
