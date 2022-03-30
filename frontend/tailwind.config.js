579b31d472d097ed38102a03a47ac67a2f0ad0af
Eonsinde <olasinde.eon@gmail.com>
2022-03-31 00:08:44 +0100

    Added Custom Spacing and App-theme

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,7 +3,17 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'app-green': '#23B123',
+        'app-green-opacity': '#23b12396',
+      },
+      spacing: {
+        '82vh': '82vh',
+        '100vh': '100vh',
+        '18vh': '18vh',
+      }
+    },
   },
   plugins: [],
 }
