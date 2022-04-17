4e67f1e76e36f364a50fba30ff7c592051fe9776
Eonsinde <olasinde.eon@gmail.com>
2022-04-17 12:59:51 +0100

    Modified: Redesigned routing patterns

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,24 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import BaseView from './layout/BaseView';
 
+import Login from './forms/Login';
+import Register from './forms/Register';
+import PageNotFound from './layout/PageNotFound';
+
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path='/*' element={ <BaseView /> } />
+            <Route path='/*' element={ <BaseView /> } />
+
+            {/* Auth Pages aspects */}
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          
+            {/* No-matching route */}
+            <Route path='/pnf' element={<PageNotFound />} />
         </Routes>
      </Router>
   </>
