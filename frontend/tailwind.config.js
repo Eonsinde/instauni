7688ce50f0b58a29e92f6b4140c6750dd4cdf064
Eonsinde <olasinde.eon@gmail.com>
2022-04-17 12:55:40 +0100

    Added new config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,9 +7,12 @@ module.exports = {
       colors: {
         'app-green': '#23B123',
         'app-green-opacity': '#23b12396',
+        'border-dark': 'rgb(88, 88, 88, 0.74)'
       },
       spacing: {
         '82vh': '82vh',
+        '70vh': '70vh',
+        '12vh': '12vh',
         '100vh': '100vh',
         '18vh': '18vh',
       }
