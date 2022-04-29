4bbb6e19a103fbf2d78dcd27891a340f9ba27511
eonsinde <olasinde.eon@gmail.com>
2022-04-29 02:55:31 +0100

    Added form validation support

---
+++ frontend/src/forms/Contact.js
@@ -0,0 +1,98 @@
+import {useState } from 'react';
+import { useForm } from "react-hook-form";
+import { motion } from 'framer-motion';
+
+
+const Contact = () => {
+    let [formData, setFormData] = useState({
+        "full_name": "",
+        "email": "",
+        "title": "",
+        "message": "",
+    });
+
+    const { register:RegisterField, handleSubmit:SubmitForm, formState: { errors } } = useForm(); // form validation
+
+    const handleChange = e => {
+        setFormData(prevState => ({
+            ...prevState,
+            [e.target.name] : e.target.value
+        }));
+    }
+
+    const handleSubmit = () => {
+        
+    }
+
+
+    return ( 
+        <motion.div
+            initial={{ translateX: '-100%' }} 
+            animate={{ translateX: 0}} 
+            exit={{ translateX: '-100%' }}
+            transition={{ duration: 0.5 }}
+        >
+        <section className='h-88vh overflow-hidden'>
+            <div className='container mx-auto h-full'>
+                <div className='h-full flex justify-center items-start lg:mt-28 mt-28'>
+                    <form onSubmit={SubmitForm(handleSubmit)} className='flex flex-col px-2 w-10/12 md:w-8/12 lg:w-5/12 mx-auto'> 
+                        <div className='form-sect mb-5 md:mb-3 lg:mb-4'>
+                            <input 
+                                type='text' 
+                                onChange={handleChange}
+                                {...RegisterField("full_name", {required: true, maxLength: 50})} 
+                                className={`p-3 w-full border-solid border-2 border-gray-500 rounded-md shadow-sm focus:border-gray-200 
+                                    focus:ring-4 focus:ring-gray-200  ${(errors.full_name?.type === 'required' || errors.full_name?.type === 'maxLength') ? 'focus:border-red-200 focus:ring-red-400' : '' } 
+                                    transition ease-in-out delay-150`}   
+                                placeholder='Full Name *' 
+                            />
+                            {/* <p className='text-sm text-slate-500 px-2'><span className='text-app-green'>NB</span>: Person to delivery to(if you - write your info)</p> */}
+                        </div>
+                        <div className='form-sect mb-5 md:mb-3 lg:mb-4'>
+                            <input 
+                                type='email' 
+                                onChange={handleChange}
+                                {...RegisterField("email", {required: true, maxLength: 50})} 
+                                className={`p-3 w-full border-solid border-2 border-gray-500 rounded-md shadow-sm focus:border-gray-200 
+                                    focus:ring-4 focus:ring-gray-200  ${(errors.email?.type === 'required' || errors.email?.type === 'maxLength') ? 'focus:border-red-200 focus:ring-red-400' : '' } 
+                                    transition ease-in-out delay-150`}    
+                                placeholder="Email *" 
+                            />
+                            {/* <p className='text-sm text-slate-500 px-2'><span className='text-app-green'>NB</span>: Where item is located</p> */}
+                        </div>
+                        <div className='form-sect mb-5 md:mb-3 lg:mb-4'>
+                            <input 
+                                type='text' 
+                                onChange={handleChange}
+                                {...RegisterField("title", {required: true, maxLength: 50})} 
+                                className={`p-3 w-full border-solid border-2 border-gray-500 rounded-md shadow-sm focus:border-gray-200 
+                                    focus:ring-4 focus:ring-gray-200  ${(errors.title?.type === 'required' || errors.title?.type === 'maxLength') ? 'focus:border-red-200 focus:ring-red-400' : '' } 
+                                    transition ease-in-out delay-150`}   
+                                placeholder="Title *" 
+                            />
+                            {/* <p className='text-sm text-slate-500 px-2'><span className='text-app-green'>NB</span>: Where item should be delivered</p> */}
+
+                        </div>
+                        <div className='form-sect mb-5 md:mb-3 lg:mb-4'>
+                            <textarea 
+                                style={{maxHeight:'100px'}}
+                                onChange={handleChange}
+                                {...RegisterField("message", {required: true, maxLength: 50})} 
+                                className={`p-3 w-full border-solid border-2 border-gray-500 rounded-md shadow-sm focus:border-gray-200 
+                                    focus:ring-4 focus:ring-gray-200  ${(errors.message?.type === 'required' || errors.message?.type === 'maxLength') ? 'focus:border-red-200 focus:ring-red-400' : '' } 
+                                    transition ease-in-out delay-150`}   
+                                placeholder="Message *"
+                            ></textarea>
+                            {/* <p className='text-sm text-slate-500 px-2'><span className='text-app-green'>NB</span>: Detailed description of task, like the location of the items</p> */}
+                        </div>
+                        <button type='submit' className='bg-app-green text-white p-3 mt-0 md:mt-2 rounded-md shadow-sm focus:ring-4 focus:ring-green-200 transition ease-in-out delay-150'>Submit</button>
+                        {/* <p className='text-center mt-7 md:mt-3 lg:mt-6 text-gray-800'>Return to <Link to='/objectives' className='text-app-green hover:text-app-green'>Objectives</Link></p> */}
+                    </form>
+                </div>
+            </div>
+        </section>
+        </motion.div>
+    );
+}
+ 
+export default Contact;
--- frontend/src/forms/CreateTask.js
+++ frontend/src/forms/CreateTask.js
@@ -1,9 +1,31 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { useForm } from "react-hook-form";
 import { motion } from 'framer-motion';
 
 
 const CreateTask = () => {
+    let [formData, setFormData] = useState({
+        "recipient_name": "",
+        "detail": "",
+        "delivery_location": "",
+        "item_location": "",
+        "price_offer": 0.0,
+        "status": ""
+    });
+    
+    const { register:RegisterField, handleSubmit:SubmitForm, formState: { errors } } = useForm(); // form validation
+
+    const handleChange = e => {
+        setFormData(prevState => ({
+            ...prevState,
+            [e.target.name] : e.target.value
+        }));
+    }
+
+    const handleSubmit = () => {
+
+    }
+
     return ( 
         <motion.div
             initial={{ translateX: '100%' }} 
@@ -11,34 +33,73 @@ const CreateTask = () => {
             exit={{ translateX: '100%' }}
             transition={{ duration: 0.5 }}
         >
-        <section className='py-28'>
-            <div className='container flex justify-center items-center mx-auto h-96'>
-                <div className='basis-full'>
-                    <form onSubmit={e => e.preventDefault()} className='flex flex-col px-2 w-10/12 md:w-8/12 lg:w-5/12 mx-auto'> 
+        <section className='h-88vh overflow-hidden'>
+            <div className='container mx-auto h-full'>
+                <div className='h-full flex justify-center items-start mt-28'>
+                    <form onSubmit={SubmitForm(handleSubmit)} className='flex flex-col px-2 w-10/12 md:w-8/12 lg:w-5/12 mx-auto'> 
                         <div className='form-sect mb-5 md:mb-3 lg:mb-4'>
-                            <input type='text' className='p-3 mb-1 w-full border-solid border-2 border-gray-500 rounded-md shadow-sm focus:border-gray-200 focus:ring-4 focus:ring-gray-200 transition ease-in-out delay-150' placeholder='Full Name' />
-                            <p className='text-sm text-slate-500 px-2'><span className='text-app-green'>NB</span>: Person to delivery to(if you - write your info)</p>
+                            <input 
+                                type='text'
+                                onChange={handleChange} 
+                                {...RegisterField("recipient_name", {required: true, maxLength: 50})} 
+                                className={`p-3 w-full border-solid border-2 border-gray-500 rounded-md shadow-sm focus:border-gray-200 
+                                    focus:ring-4 focus:ring-gray-200  ${(errors.detail?.type === 'required' || errors.detail?.type === 'maxLength') ? 'focus:border-red-200 focus:ring-red-400' : '' } 
+                                    transition ease-in-out delay-150`}  
+                                placeholder='Full Name *' 
+                            />
+                            {/* <p className='text-sm text-slate-500 px-2'><span className='text-app-green'>NB</span>: Person to delivery to(if you - write your info)</p> */}
                         </div>
                         <div className='form-sect mb-5 md:mb-3 lg:mb-4'>
-                            <textarea style={{height:'100px'}} className='p-3 mb-1 w-full border-solid border-2 border-gray-500 rounded-md shadow-sm focus:border-gray-200 focus:ring-4 focus:ring-gray-200 transition ease-in-out delay-150'  placeholder="Write Objective/Task"></textarea>
-                            <p className='text-sm text-slate-500 px-2'><span className='text-app-green'>NB</span>: Detailed description of task, like the location of the items</p>
+                            <textarea 
+                                style={{maxHeight:'100px'}} 
+                                onChange={handleChange}
+                                {...RegisterField("detail", {required: true, maxLength: 500})} 
+                                className={`p-3 w-full border-solid border-2 border-gray-500 rounded-md shadow-sm focus:border-gray-200 
+                                    focus:ring-4 focus:ring-gray-200  ${(errors.detail?.type === 'required' || errors.detail?.type === 'maxLength') ? 'focus:border-red-200 focus:ring-red-400' : '' } 
+                                    transition ease-in-out delay-150`}    
+                                placeholder="Write Objective/Task *"
+                            >
+                            </textarea>
+                            {/* <p className='text-sm text-slate-500 px-2'><span className='text-app-green'>NB</span>: Detailed description of task, like the location of the items</p> */}
                         </div>
                         <div className='form-sect mb-5 md:mb-3 lg:mb-4'>
-                            <input type='text' className='p-3 mb-1 w-full border-solid border-2 border-gray-500 rounded-md shadow-sm focus:border-gray-200 focus:ring-4 focus:ring-gray-200 transition ease-in-out delay-150' placeholder="Item's Location" />
-                            <p className='text-sm text-slate-500 px-2'><span className='text-app-green'>NB</span>: Where item is located</p>
-
+                            <input 
+                                type='text'
+                                onChange={handleChange} 
+                                {...RegisterField("item_location", {maxLength: 50})} 
+                                className={`p-3 w-full border-solid border-2 border-gray-500 rounded-md shadow-sm focus:border-gray-200 
+                                    focus:ring-4 focus:ring-gray-200  ${(errors.item_location?.type === 'maxLength') ? 'focus:border-red-200 focus:ring-red-400' : '' } 
+                                    transition ease-in-out delay-150`}   
+                                placeholder="Item's Location" 
+                            />
+                            {/* <p className='text-sm text-slate-500 px-2'><span className='text-app-green'>NB</span>: Where item is located</p> */}
                         </div>
                         <div className='form-sect mb-5 md:mb-3 lg:mb-4'>
-                            <input type='text' className='p-3 mb-1 w-full border-solid border-2 border-gray-500 rounded-md shadow-sm focus:border-gray-200 focus:ring-4 focus:ring-gray-200 transition ease-in-out delay-150' placeholder="Delivery Location" />
-                            <p className='text-sm text-slate-500 px-2'><span className='text-app-green'>NB</span>: Where item should be delivered</p>
-
+                            <input 
+                                type='text'
+                                onChange={handleChange} 
+                                {...RegisterField("delivery_location", {maxLength: 50})} 
+                                className={`p-3 w-full border-solid border-2 border-gray-500 rounded-md shadow-sm focus:border-gray-200 
+                                    focus:ring-4 focus:ring-gray-200  ${(errors.delivery_location?.type === 'maxLength') ? 'focus:border-red-200 focus:ring-red-400' : '' } 
+                                    transition ease-in-out delay-150`}   
+                                placeholder="Delivery Location" 
+                            />
+                            {/* <p className='text-sm text-slate-500 px-2'><span className='text-app-green'>NB</span>: Where item should be delivered</p> */}
                         </div>
                         <div className='form-sect mb-5 md:mb-3 lg:mb-4'>
-                            <input type='number' className='p-3 mb-1 w-full border-solid border-2 border-gray-500 rounded-md shadow-sm focus:border-gray-200 focus:ring-4 focus:ring-gray-200 transition ease-in-out delay-150' placeholder='Price Offer'/>
-                            <p className='text-sm text-slate-500 px-2'><span className='text-app-green'>NB</span>: Price for task fulfillment</p>
+                            <input 
+                                type='number'
+                                onChange={handleChange} 
+                                {...RegisterField("price_offer", {required: true})} 
+                                className={`p-3 w-full border-solid border-2 border-gray-500 rounded-md shadow-sm focus:border-gray-200 
+                                    focus:ring-4 focus:ring-gray-200  ${(errors.price_offer?.type === 'required') ? 'focus:border-red-200 focus:ring-red-400' : '' } 
+                                    transition ease-in-out delay-150`}   
+                                placeholder='Price Offer *'
+                            />
+                            {/* <p className='text-sm text-slate-500 px-2'><span className='text-app-green'>NB</span>: Price for task fulfillment</p> */}
                         </div>
                         <button type='submit' className='bg-app-green text-white p-3 mt-0 md:mt-2 rounded-md shadow-sm focus:ring-4 focus:ring-green-200 transition ease-in-out delay-150'>Create Task</button>
-                        <p className='text-center mt-7 md:mt-3 lg:mt-6 text-gray-800'>Return to <Link to='/objectives' className='text-app-green'>Objectives</Link></p>
+                        {/* <p className='text-center mt-7 md:mt-3 lg:mt-6 text-gray-800'>Return to <Link to='/objectives' className='text-app-green hover:text-app-green'>Objectives</Link></p> */}
                     </form>
                 </div>
             </div>
