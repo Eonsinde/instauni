fab407a4b3b7cf06984121fb63bdab6f01807de8
eonsinde <olasinde.eon@gmail.com>
2022-04-29 02:31:46 +0100

    Added JWT support

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
 from accounts.api import *
 
 
 urlpatterns = [
-    # path('api/auth/register', RegisterAPI.as_view()),
+    path('api/auth/register', RegisterAPI.as_view()),
+    path('api/auth/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
     path('api/auth/users', UserListAPI.as_view()),
     path('api/auth/login', LoginAPI.as_view()),
     path('api/auth/user', UserAPI.as_view()),
