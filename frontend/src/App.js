bd7c7ff2bc841a6131f3c7d1694603297769a2dd
eonsinde <olasinde.eon@gmail.com>
2022-04-29 02:33:17 +0100

    Implemented Redux Provider, store and alert system

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './store';
+
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import { AuthProvider } from './context/AuthContext';
 
 import BaseView from './layout/BaseView';
 
@@ -9,20 +16,24 @@ import PageNotFound from './layout/PageNotFound';
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <Router>
-        <Routes>
-            <Route path='/*' element={ <BaseView /> } />
+        <AuthProvider>
+          <Routes>
+              <Route path='/*' element={ <BaseView /> } />
 
-            {/* Auth Pages aspects */}
-            <Route path='/login' element={<Login />} />
-            <Route path='/register' element={<Register />} />
-          
-            {/* No-matching route */}
-            <Route path='/pnf' element={<PageNotFound />} />
-        </Routes>
+              {/* Auth Pages aspects */}
+              <Route path='/login' element={<Login />} />
+              <Route path='/register' element={<Register />} />
+            
+              {/* No-matching route */}
+              <Route path='/pnf' element={<PageNotFound />} />
+              <Route path='*' element={<PageNotFound />} />
+          </Routes>
+        </AuthProvider>
      </Router>
-  </>
+     <ToastContainer />
+  </Provider>
   );
 }
 
