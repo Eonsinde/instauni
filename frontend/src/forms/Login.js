a509f9c560eeb116abd883bfcdeb6c2f6e1fea80
eonsinde <olasinde.eon@gmail.com>
2022-04-29 02:42:13 +0100

    Added form validation and implemented operations

--- frontend/src/forms/Login.js
+++ frontend/src/forms/Login.js
@@ -1,45 +1,109 @@
 import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate, useLocation } from 'react-router-dom';
+import { useForm } from "react-hook-form";
+import { login, reset } from '../reducers/auth/authSlice';
 import { Link } from 'react-router-dom';
-import loginImg from '../Assets/images/login.png'
+import { toast } from 'react-toastify';
+
+import loginImg from '../Assets/images/login.png';
 
 
 const Login = () => {
-    let [username, setUsername] = useState('');
-    let [password, setPassword] = useState('');
+    let [formData, setFormData] = useState({
+        username: '',
+        password: ''
+    });
 
-    const handleSubmit = e => {
-        // before A
-        console.log("Make AJAX request to submit username and password to server", username, password);
+    const { register:RegisterField, handleSubmit:SubmitForm, formState: { errors } } = useForm(); // form validation
+
+    const navigate = useNavigate();
+    const location = useLocation();
+    const dispatch  = useDispatch();
+
+    const from = location.state?.from?.pathname;
+
+    const { user, isLoading, isError, isSuccess, message } = useSelector(state => state.auth);
+
+    useEffect(() => {
+        if (isError){
+            toast.error(message);
+            dispatch(reset());
+        }
+
+        if (user || isSuccess){
+            if (from) // navigate to protected view 
+                navigate(from, { replace: true });
+            toast.success(`Logged In: Welcome ${user.username}`);
+            dispatch(reset());
+        }
+    }, [user, isLoading, isError, isSuccess, message, navigate, dispatch]);
+
+    const handleChange = e => {
+        setFormData(prevState => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        }));
     }
 
+    const handleSubmit = () => {
+        // login user
+        dispatch(login(formData));
+    }
+
+    // we can also use navigate here incase of any issues
+    if (user || isSuccess)
+        navigate('/');
     return (
-        <>
-        <header style={{height: '18vh'}} className="">
-            <div className='container h-full mx-auto flex justify-center items-center relative'>
-                <Link to='/' className='text-gray-800 text-5xl font-semibold hover:text-gray-800'>Insta<span className="text-app-green">life</span></Link>
-            </div>
-        </header>
-        <section className='py-28'>
-            <div className='container flex justify-center items-center mx-auto h-96'>
-                <div className='hidden md:block md:px-3 md:basis-1/2'>
-                    <img className="mx-auto" src={loginImg} alt="" width={'100%'} height={'100%'} />
+            <>
+            <header style={{height: '18vh'}} className="">
+                <div className='container h-full mx-auto flex justify-center items-center relative'>
+                    <Link to='/' className='text-gray-800 md:text-5xl text-4xl font-semibold hover:text-gray-800'>Insta<span className="text-app-green">life</span></Link>
                 </div>
-                <div className='basis-full md:basis-1/2'>
-                    <form onSubmit={handleSubmit} className='flex flex-col px-2 w-10/12 md:w-9/12 lg:w-7/12 mx-auto'> 
-                        <div className='form-sect mb-7 md:mb-3 lg:mb-5'>
-                            <input type='text' onChange={e => setUsername(e.target.value)} className='p-3 w-full border-solid border-2 border-gray-500 rounded-md shadow-sm focus:border-gray-200 focus:ring-4 focus:ring-gray-200 transition ease-in-out delay-150' placeholder='Email Or Registration Number' />
-                        </div>
-                        <div className='form-sect mb-7 md:mb-3 lg:mb-5'>
-                            <input type='text' onChange={e => setPassword(e.target.value)} className='p-3 w-full border-solid border-2 border-gray-500 rounded-md shadow-sm focus:border-gray-200 focus:ring-4 focus:ring-gray-200 transition ease-in-out delay-150' placeholder='Password'/>
-                        </div>
-                        <button type='submit' className='bg-app-green text-white p-3 rounded-md shadow-sm focus:ring-4 focus:ring-green-200 transition ease-in-out delay-150'>Log In</button>
-                        <p className='text-center mt-7 md:mt-3 lg:mt-6'>Don't have an account? <Link to='/register' className='text-app-green'>Sign Up</Link></p>
-                    </form>
+            </header>
+            <section className='py-28'>
+                <div className='container flex justify-center items-center mx-auto h-96'>
+                    <div className='hidden md:block md:px-3 md:basis-1/2'>
+                        <img className="mx-auto" src={loginImg} alt="" width={'100%'} height={'100%'} />
+                    </div>
+                    <div className='basis-full md:basis-1/2'>
+                        <form onSubmit={SubmitForm(handleSubmit)} className='flex flex-col px-2 w-10/12 md:w-9/12 lg:w-7/12 mx-auto'> 
+                            <div className='form-sect mb-7 md:mb-3 lg:mb-5'>
+                                <input 
+                                    type='text' 
+                                    {...RegisterField("username", {required: true, maxLength: 50})} 
+                                    onChange={handleChange} 
+                                    className={`p-3 w-full border-solid border-2 border-gray-500 rounded-md shadow-sm focus:border-gray-200 
+                                            focus:ring-4 focus:ring-gray-200  ${errors.username?.type === 'required' ? 'focus:border-red-200 focus:ring-red-400' : '' } 
+                                            transition ease-in-out delay-150`} 
+                                    placeholder='Username'  
+                                />
+                            </div>
+                            <div className='form-sect mb-7 md:mb-3 lg:mb-5'>
+                                <input 
+                                    type='password' 
+                                    {...RegisterField("password", { required: true, maxLength: 255 })} 
+                                    onChange={handleChange} 
+                                    className={`p-3 w-full border-solid border-2 border-gray-500 rounded-md shadow-sm focus:border-gray-200 
+                                            focus:ring-4 focus:ring-gray-200  ${errors.password?.type === 'required' ? 'focus:border-red-200 focus:ring-red-400' : '' } 
+                                            transition ease-in-out delay-150`} 
+                                    placeholder='Password'
+                                />
+                            </div>
+                            <button 
+                                disabled={isLoading ? true : false}
+                                type='submit' 
+                                className={`${!isLoading ? 'bg-app-green' : 'bg-app-green-opacity animate-pulse'} text-white p-3 rounded-md shadow-sm focus:ring-4 focus:ring-green-200 transition ease-in-out delay-150`}
+                            >
+                                {isLoading ? 'Submitting' : 'Login'}
+                            </button>
+                            <p className='text-center mt-7 md:mt-3 lg:mt-6'>Don't have an account? <Link to='/register' className='text-app-green hover:text-app-green'>Sign Up</Link></p>
+                        </form>
+                    </div>
                 </div>
-            </div>
-        </section>
-        </>
-    );
+            </section>
+            </>
+        );
 }
  
 export default Login;
--- frontend/src/forms/Register.js
+++ frontend/src/forms/Register.js
@@ -1,50 +1,167 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate, Navigate } from 'react-router-dom'
+import { useForm } from "react-hook-form";
+import { toast } from 'react-toastify'
+import { register, reset } from '../reducers/auth/authSlice'
 import registerImg from '../Assets/images/register.png'
 
 
 const Register = () => {
+    const [formData, setFormData] = useState({
+        email: "",
+        username: "",
+        reg_no: "",
+        level: "",
+        gender: "",
+        password: "",
+        confirm_password: ""
+    });
+
+    const { register:RegisterField, handleSubmit:SubmitForm, formState: { errors } } = useForm(); // form validation
+
+    const navigate = useNavigate();
+    const dispatch  = useDispatch();
+
+    const { user, isLoading, isError, isSuccess, message } = useSelector(state => state.auth);
+
+    useEffect(() => {
+        if (isError) {
+            toast.error(message);
+            dispatch(reset());
+        }
+
+        if (user || isSuccess || message.message === "success"){
+            navigate('/login');
+            toast.success("Account has been registered - Login to continue");
+            dispatch(reset());
+        }
+    }, [user, isLoading, isError, isSuccess, message, navigate, dispatch]);
+
+    const handleChange = e => {
+        setFormData(prevState => ({
+            ...prevState,
+            [e.target.name]: e.target.value.trim()
+        }));
+    }
+
+    const handleSubmit = () => {
+        if (formData.password !== formData.confirm_password) {
+            toast.error("Password Don't Match");
+        } else {
+            const userData = {
+                ...formData
+            };
+
+            dispatch(register(userData));
+        }
+    }
+
+    if (user || isSuccess)
+        return <Navigate to='/' />;
     return ( 
         <>
         <header style={{height: '18vh'}} className="">
             <div className='container h-full mx-auto flex justify-center items-center relative'>
-                <Link to='/' className='text-gray-800 text-5xl font-semibold hover:text-gray-800'>Insta<span className="text-app-green">life</span></Link>
+                <Link to='/' className='text-gray-800 md:text-5xl text-4xl font-semibold hover:text-gray-800'>Insta<span className="text-app-green">life</span></Link>
             </div>
         </header>
-        <section className='h-82vh overflow-hidden'>
+        <section className='h-82vh overflow-hidden py-2'>
             <div className='container flex justify-center items-center mx-auto'>
                 <div className='hidden md:block md:px-3 md:basis-1/2'>
                     <img className="mx-auto" src={registerImg} alt="" width={'100%'} height={'100%'} />
                 </div>
                 <div className='basis-full md:basis-1/2'>
-                    <form onSubmit={e => e.preventDefault()} className='flex flex-col px-2 w-10/12 md:w-9/12 lg:w-7/12 mx-auto'> 
-                        <div className='form-sect mb-7 md:mb-3 lg:mb-5'>
-                            <input type='text' className='p-3 w-full border-solid border-2 border-gray-500 rounded-md shadow-sm focus:border-gray-200 focus:ring-4 focus:ring-gray-200 transition ease-in-out delay-150' placeholder='Full Name' />
+                    <form onSubmit={SubmitForm(handleSubmit)} className='flex flex-col px-2 w-10/12 md:w-9/12 lg:w-7/12 mx-auto'> 
+                         <div className='form-sect mb-7 md:mb-3 lg:mb-5'>
+                            <input 
+                                {...RegisterField("email", { pattern: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, required: true })} 
+                                className={`p-3 w-full border-solid border-2 border-gray-500 rounded-md shadow-sm 
+                                    focus:border-gray-200 focus:ring-4 focus:ring-gray-200  
+                                    ${(errors.email?.type === 'required' || errors.email?.type === 'pattern') ? 'focus:border-red-200 focus:ring-red-400' : '' } 
+                                    transition ease-in-out delay-150`}
+                                onChange={handleChange} 
+                                placeholder='Email Address' 
+                            />
+                            {/* {  (errors.email?.type === 'pattern') && toast.error("Email: Required Field & must be valid") } */}
                         </div>
                         <div className='form-sect mb-7 md:mb-3 lg:mb-5'>
-                            <input type='email' className='p-3 w-full border-solid border-2 border-gray-500 rounded-md shadow-sm focus:border-gray-200 focus:ring-4 focus:ring-gray-200 transition ease-in-out delay-150' placeholder='Email' />
+                            <input 
+                                {...RegisterField('username', { maxLength:50, required: true })} 
+                                className={`p-3 w-full border-solid border-2 border-gray-500 rounded-md shadow-sm 
+                                    focus:border-gray-200 focus:ring-4 focus:ring-gray-200 
+                                    ${errors.username?.type === 'required' ? 'focus:border-red-200 focus:ring-red-400' : '' } 
+                                    transition ease-in-out delay-150`}
+                                onChange={handleChange} 
+                                placeholder='Username' 
+                            />
                         </div>
                         <div className='form-sect mb-7 md:mb-3 lg:mb-5'>
-                            <input type='text' className='p-3 w-full border-solid border-2 border-gray-500 rounded-md shadow-sm focus:border-gray-200 focus:ring-4 focus:ring-gray-200' placeholder='Registration Number' />
+                            <input
+                                {...RegisterField('reg_no', { minLength: 7, maxLength: 7, required: true })} 
+                                className={`p-3 w-full border-solid border-2 border-gray-500 rounded-md shadow-sm 
+                                    focus:border-gray-200 focus:ring-4 focus:ring-gray-200 
+                                    ${(errors.reg_no?.type === 'required' || errors.reg_no?.type === 'minLength') ? 'focus:border-red-200 focus:ring-red-400' : '' } 
+                                    transition ease-in-out delay-150 `}  
+                                onChange={handleChange} placeholder='Registration Number' 
+                            />
                         </div>
                         <div className='form-sect mb-7 md:mb-3 lg:mb-5'>
-                            <input type='text' className='p-3 w-full border-solid border-2 border-gray-500 rounded-md shadow-sm focus:border-gray-200 focus:ring-4 focus:ring-gray-200 transition ease-in-out delay-150' placeholder='Level' />
+                            <input
+                                {...RegisterField("level", { minLength: 3, maxLength: 3, required: true })} 
+                                className={`p-3 w-full border-solid border-2 border-gray-500 rounded-md shadow-sm 
+                                    focus:border-gray-200 focus:ring-4 focus:ring-gray-200 
+                                    ${(errors.reg_no?.type === 'required' || errors.reg_no?.type === 'minLength') ? 'focus:border-red-200 focus:ring-red-400' : '' } 
+                                    transition ease-in-out delay-150  `} 
+                                onChange={handleChange} 
+                                placeholder='Level'
+                            />
                         </div>
                         <div className='form-sect mb-7 md:mb-3 lg:mb-5'>
-                            <select className='p-3 w-full border-solid border-2 border-gray-500 rounded-md shadow-sm focus:border-gray-200 focus:ring-4 focus:ring-gray-200 transition ease-in-out delay-150'>
+                            <select 
+                                {...RegisterField('gender', { required: true })} 
+                                className={`p-3 w-full border-solid border-2 border-gray-500 rounded-md shadow-sm 
+                                    focus:border-gray-200 focus:ring-4 focus:ring-gray-200 
+                                    ${errors.gender?.type === 'required' ? 'focus:border-red-200 focus:ring-red-400' : '' }
+                                    transition ease-in-out delay-150`} 
+                                onChange={handleChange}
+                            >
                                 <option value=''>Gender</option>
                                 <option value='m'>Male</option>
                                 <option value='f'>Female</option>
                             </select>
                         </div>
                         <div className='form-sect mb-7 md:mb-3 lg:mb-5'>
-                            <input type='password' className='p-3 w-full border-solid border-2 border-gray-500 rounded-md shadow-sm focus:border-gray-200 focus:ring-4 focus:ring-gray-200 transition ease-in-out delay-150' placeholder='Password'/>
+                            <input 
+                                type='password' 
+                                {...RegisterField("password", { maxLength: 255, required: true })} 
+                                className={`p-3 w-full border-solid border-2 border-gray-500 rounded-md shadow-sm 
+                                    focus:border-gray-200 focus:ring-4 focus:ring-gray-200 
+                                    transition ease-in-out delay-150  ${errors.password?.type === 'required' ? 'focus:border-red-200 focus:ring-red-400' : '' } `} 
+                                onChange={handleChange} 
+                                placeholder='Password'
+                            />
                         </div>
                         <div className='form-sect mb-7 md:mb-3 lg:mb-5'>
-                            <input type='password' className='p-3 w-full border-solid border-2 border-gray-500 rounded-md shadow-sm focus:border-gray-200 focus:ring-4 focus:ring-gray-200 transition ease-in-out delay-150' placeholder='Confirm Password'/>
+                            <input 
+                                type='password' 
+                                {...RegisterField("confirm_password", { maxLength: 255, required: true })} 
+                                className={`p-3 w-full border-solid border-2 border-gray-500 rounded-md shadow-sm 
+                                    focus:border-gray-200 focus:ring-4 focus:ring-gray-200 
+                                    transition ease-in-out delay-150  ${errors.confirm_password?.type === 'required' ? 'focus:border-red-200 focus:ring-red-400' : '' } `} 
+                                onChange={handleChange} 
+                                placeholder='Confirm Password'
+                            />
                         </div>
-                        <button type='submit' className='bg-app-green text-white p-3 rounded-md shadow-sm focus:ring-4 focus:ring-green-200 transition ease-in-out delay-150'>Register</button>
-                        <p className='text-center mt-7 md:mt-3 lg:mt-6'>Already Registered? <Link to='/login' className='text-app-green'>Sign In</Link></p>
+                        <button 
+                            disabled={isLoading ? true : false}
+                            type='submit' 
+                            className={`${!isLoading ? 'bg-app-green' : 'bg-app-green-opacity animate-pulse'} text-white p-3 rounded-md shadow-sm focus:ring-4 focus:ring-green-200 transition ease-in-out delay-150`}
+                        >
+                            {isLoading ? 'Submitting' : 'Register'}
+                        </button>
+                        <p className='text-center mt-7 md:mt-3 lg:mt-6'>Already Registered? <Link to='/login' className='text-app-green hover:text-app-green'>Sign In</Link></p>
                     </form>
                 </div>
             </div>
