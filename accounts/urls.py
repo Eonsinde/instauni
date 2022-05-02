cbd22e6dccf66154b7db71567572517518652916
eonsinde <olasinde.eon@gmail.com>
2022-05-02 12:31:23 +0100

    Updated routes - remove redundant routes

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,14 +6,10 @@ from accounts.api import *
 
 
 urlpatterns = [
+    path('api/auth/user/<int:pk>/', UserAPI.as_view()),
     path('api/auth/register', RegisterAPI.as_view()),
     path('api/auth/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
-    path('api/auth/users', UserListAPI.as_view()),
-    path('api/auth/login', LoginAPI.as_view()),
-    path('api/auth/user', UserAPI.as_view()),
-    # path('api/auth/logout', knox_views.LogoutView.as_view(), name='knox_logout'),
 ]
 
 
