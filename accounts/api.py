a4dbf3f84ed108d3400d334fc51404d53e10a7a4
eonsinde <olasinde.eon@gmail.com>
2022-05-02 12:29:31 +0100

    Updated UserAPI view and removed UserListAPI

--- accounts/api.py
+++ accounts/api.py
@@ -25,34 +25,21 @@ class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
 
 
-class UserListAPI(generics.ListCreateAPIView):
-    serializer_class = UserSerializer
-
-    def get_queryset(self):
-        return CustomUser.objects.all()
-
 class UserAPI(generics.RetrieveAPIView):
-    # permission_classes = [
-    #     permissions.IsAuthenticated
-    # ]
+    permission_classes = [
+        permissions.IsAuthenticated
+    ]
+
     serializer_class = UserSerializer
+    queryset = CustomUser.objects.all()
+    lookup_fields = 'id'
 
-    def get_object(self):
-        auth_token = self.request.COOKIES.get('jwt')
-        if not auth_token:
-            raise AuthenticationFailed("Unauthenticated!")
-        
-        try:
-            payload = jwt.decode(auth_token, 'secret', algorithms=['HS256'])
-        except jwt.ExpiredSignatureError:
-            raise AuthenticationFailed("Unauthenticated!")
-        
-        user = CustomUser.objects.get(pk=payload.get("id"))
-
-        # return Response(
-        #     UserSerializer(user).data
-        # )
-        return self.request.user
+    # def get_object(self):
+    #     user = CustomUser.objects.get(username=self.)
+    #     return Response(
+    #         UserSerializer(user).data
+    #     )
+    #     return self.request.user
 
 
 class RegisterAPI(generics.CreateAPIView):
@@ -64,30 +51,6 @@ class RegisterAPI(generics.CreateAPIView):
         user = serializer.save()
         return Response(data={"message": "success"}, status=status.HTTP_201_CREATED)
 
-
-class LoginAPI(generics.GenericAPIView):
-    serializer_class = LoginSerializer
-
-    def post(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data
-
-        payload = {
-            'id': user.id,
-            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
-            'iat': datetime.datetime.utcnow()
-        }
-
-        auth_token = jwt.encode(payload, 'secret', algorithm='HS256')
-
-        response = Response()
-        response.set_cookie(key='jwt', value=auth_token, httponly=True)
-        response.data = {
-            'user':  UserSerializer(user, context=self.get_serializer_context()).data
-        }
- 
-        return response
 
 
 
