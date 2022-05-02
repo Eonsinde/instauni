8eed4598873326356066a56d30d1c1c3b015d543
eonsinde <olasinde.eon@gmail.com>
2022-05-03 00:26:35 +0100

    Fixed issures with Token Refresh

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,7 +1,8 @@
 import { createContext, useContext, useEffect } from 'react'
-// import jwt_decode from 'jwt_decode'
-import { useNavigate, Navigate, useLocation,  } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+import { Navigate, useLocation,  } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import { updateToken, reset } from '../reducers/auth/authSlice'
+import { toast } from 'react-toastify';
 
 
 export const AuthContext = createContext();
@@ -11,14 +12,31 @@ export const useAuth = () => {
 }
 
 const AuthProvider = ({ children }) => {
-    const { user } = useSelector(state => state.auth);
-
+    const dispatch  = useDispatch();
+    const { user, auth_tokens, tokenRefreshing, tokenRefreshError, message } = useSelector(state => state.auth);
+   
     useEffect(() => {
-        // pay attention to user changing in the redux state
-    }, [user]);
+        if (tokenRefreshError){
+            toast.error(`${message}`);
+            dispatch(reset());
+        }
+
+        let fourMinutes = 1000 * 60 * 2; // 2 mins
+
+        let interval = setInterval(() => {
+            console.log("Auth Token: ", auth_tokens);
+            if(auth_tokens){
+                console.log("About to execute token refresh");
+                dispatch(updateToken(null));
+            }
+        }, fourMinutes);
+
+        return () => clearInterval(interval);
+    }, [tokenRefreshing, auth_tokens]);
 
     const contextData = {
-        user
+        user,
+        auth_tokens
     };
 
     return ( 
