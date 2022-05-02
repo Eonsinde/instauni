bdd9b674be56b72db512b01fa1a8f99d61190d73
eonsinde <olasinde.eon@gmail.com>
2022-05-02 12:37:37 +0100

    created Loaders for dashboard pages

---
+++ frontend/src/layout/Dashboard/loaders/DashLoaders.js
@@ -0,0 +1,30 @@
+
+export const ProfileDetailsLoader = () => {
+    return (
+        <div className='user-details text-center md:text-left'>
+            <div className='bg-gray-200 h-6 w-72 animate-pulse rounded mb-6'></div>
+            <div className='bg-gray-200 h-6 w-60 animate-pulse rounded mb-6'></div>
+            <div className='bg-gray-200 h-6 w-52 animate-pulse rounded mb-6'></div>
+            <div className='bg-gray-200 h-6 w-40 animate-pulse rounded mb-6'></div>
+            <div className='bg-gray-200 h-6 w-32 animate-pulse rounded mb-6'></div>
+            <div className='bg-gray-200 h-6 w-36 animate-pulse rounded mb-6'></div>
+            <div className='bg-gray-200 h-6 w-32 animate-pulse rounded mb-6'></div>
+            <div className='bg-gray-200 h-6 w-36 animate-pulse rounded mb-6'></div>
+            <div className='bg-gray-200 h-6 w-60 animate-pulse rounded mb-6'></div>
+            <div className='bg-gray-200 h-6 w-64 animate-pulse rounded mb-6'></div>
+            <div className='bg-gray-200 h-6 w-72 animate-pulse rounded mb-6'></div>
+        </div>
+    )
+}
+
+export const ProfileImageLoader = () => {
+    return (
+        <figure className='flex flex-col justify-center items-center space-y-3 mb-8 md:mb-0'>
+            {
+            
+                <div className='bg-gray-200 p-24 rounded-full animate-pulse'></div>
+            }
+            <div className='bg-gray-200 h-6 w-24 animate-pulse rounded mb-6'></div>
+        </figure>
+    )
+}
