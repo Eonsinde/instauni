bd45a9385e70e10d85c0d5d9b3c809378db6529a
eonsinde <olasinde.eon@gmail.com>
2022-05-02 12:41:24 +0100

    Removed router and updated routes

--- instauni/urls.py
+++ instauni/urls.py
@@ -1,11 +1,26 @@
+from django.urls import path, include
 from rest_framework import routers
 from instauni.api import *
 
 # this is where our routes are defined 
 
-router = routers.DefaultRouter()
-router.register('tasks', TaskViewSet, 'tasks')
-# router.register('contact', ContactAPI, 'contact')
-# router.register('FAQs', FAQAPI, 'FAQs')
 
-urlpatterns = router.urls
+task_list = TaskViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+
+task_detail = TaskViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
+
+
+urlpatterns = [
+    path('tasks', task_list, name='task-list'),
+    path('tasks/<int:pk>/', task_detail, name='task-detail'),
+    path('contact', ContactAPI.as_view(), name='contact-create'),
+    path('faqs', FAQAPI.as_view(), name='faq-list'),
+]
