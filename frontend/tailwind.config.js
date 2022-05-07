7ccf850b81b5b07a78c20efaa5119fca622da626
eonsinde <olasinde.eon@gmail.com>
2022-05-07 12:37:22 +0100

    Added dark mode config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,13 @@
 module.exports = {
+  darkMode: 'class',
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
       colors: {
+        'app-dark': 'rgb(22, 22, 22)',
+        'app-dark-opacity': 'rgb(27, 27, 27)',
         'app-green': '#23B123',
         'app-green-opacity': '#23b12396',
         'border-dark': 'rgb(88, 88, 88, 0.74)'
