9e6a419d7c287cf6f54948d917cfcc1a228a234f
eonsinde <olasinde.eon@gmail.com>
2022-05-10 07:44:04 +0100

    Added new loader and made loader responsive

--- frontend/src/layout/Dashboard/loaders/DashLoaders.js
+++ frontend/src/layout/Dashboard/loaders/DashLoaders.js
@@ -1,7 +1,7 @@
 
 export const ProfileDetailsLoader = () => {
     return (
-        <div className='user-details text-center md:text-left'>
+        <div className='user-details flex flex-col justify-start items-center md:items-start'>
             <div className='bg-gray-200 h-6 w-72 animate-pulse rounded mb-6'></div>
             <div className='bg-gray-200 h-6 w-60 animate-pulse rounded mb-6'></div>
             <div className='bg-gray-200 h-6 w-52 animate-pulse rounded mb-6'></div>
@@ -28,3 +28,43 @@ export const ProfileImageLoader = () => {
         </figure>
     )
 }
+
+export const DashSpinnerLoader = (props) => {
+    return ( 
+        <div className="w-full h-72 md:h-52 flex justify-center items-center">
+            <svg
+                style={{...styles.svg}}
+                xmlns="http://www.w3.org/2000/svg"
+                x="0px"
+                y="0px"
+                viewBox="0 0 100 100"
+                xmlSpace="preserve"
+                {...props}
+            >
+                <path
+                    fill="#23B123"
+                    d="M73 50c0-12.7-10.3-23-23-23S27 37.3 27 50m3.9 0c0-10.5 8.5-19.1 19.1-19.1S69.1 39.5 69.1 50"
+                >
+                    <animateTransform
+                        attributeName="transform"
+                        attributeType="XML"
+                        type="rotate"
+                        dur="1s"
+                        from="0 50 50"
+                        to="360 50 50"
+                        repeatCount="indefinite"
+                    />
+                </path>
+            </svg>
+        </div> 
+    );
+}
+
+const styles = {
+    svg: {
+        width: '150px',
+        height: '150px',
+        display: 'block'
+    }
+}
+ 
