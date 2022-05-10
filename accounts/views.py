b15f2af794a2141aa23753d2fcfbfbfcedd0dee1
eonsinde <olasinde.eon@gmail.com>
2022-05-10 07:38:19 +0100

    Created function to handle temporal file upload

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.core.files.uploadedfile import TemporaryUploadedFile
 
 # Create your views here.
+
+
+def handle_temp_file_upload(request):
+    pass
+
