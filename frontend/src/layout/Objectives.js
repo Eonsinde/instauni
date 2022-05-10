a89a95f670135a30726f4e3767c2fdeeb3f30707
eonsinde <olasinde.eon@gmail.com>
2022-05-10 07:42:34 +0100

    Added dark mode suport

--- frontend/src/layout/Objectives.js
+++ frontend/src/layout/Objectives.js
@@ -23,30 +23,36 @@ const Objectives = () => {
                 <div className='obj-wrapper py-10 flex justify-center items-center'>{/* had to include this div to style scroll bar and allow for y-overflow */}
                     <div className='flex flex-wrap flex-col md:flex-row md:items-start md:h-full md:w-11/12 mx-auto'>
                         <div className='text-center basis-full mb-10 md:mb-8'>
-                            <h4 className='font-semibold text-xl'>I want to</h4>
+                            <h4 className='font-semibold text-xl dark:text-white'>I want to</h4>
                         </div>
                         <div className='obj basis-1/2 flex flex-col items-center justify-center text-center mb-10 md:mb-0'>
-                            <Link to='/create-task' className='border-2 border-gray-700 rounded-full p-4 hover:bg-app-green hover:text-white hover:border-app-green hover:ring-8 hover:ring-green-200 transition'>
+                            <Link 
+                                to='/create-task' 
+                                className='border-2 border-gray-700 rounded-full p-4 hover:bg-app-green hover:text-white hover:border-app-green hover:ring-8 hover:ring-green-200 dark:text-white transition'
+                            >
                                 {/* <img className="mx-auto h-48 w-48 md:h-42 md:w-42 lg:h-72 lg:w-72" src={findImg} alt="" /> */}
                                 <IoAddSharp className='text-9xl' />
                             </Link>
                             {/* <BsPlusCircle /> */}
                             <div className='obj-content flex flex-col items-center justify-center mt-8'>
                                 <h4 className='obj-caption text-app-green text-xl mb-3 font-semibold'>Create</h4>
-                                <p className='obj-text'>
+                                <p className='obj-text dark:text-white'>
                                     Post a microjob for individuals <br/>around the school to find and deliver
                                 </p>
                             </div>
                         </div>
                         <div className='obj basis-1/2 flex flex-col items-center justify-center text-center mb-0 md:mb-0'>
-                            <Link to='/find-task' className='border-2 border-gray-700 rounded-full p-4 hover:bg-app-green hover:text-white hover:border-app-green hover:ring-8 hover:ring-green-200 transition'>
+                            <Link 
+                                to='/find-task' 
+                                className='border-2 border-gray-700 rounded-full p-4 hover:bg-app-green hover:text-white hover:border-app-green hover:ring-8 hover:ring-green-200 dark:text-white transition'
+                            >
                                 {/* <img className="mx-auto h-48 w-48 md:h-42 md:w-42 lg:h-72 lg:w-72" src={findImg} alt="" /> */}
                                 <GiBinoculars className='text-9xl' />
                             </Link>
                             {/* <MdOutlineManageSearch className='text-9xl' /> */}
                             <div className='obj-content flex flex-col items-center justify-center mt-8'>
                                 <h4 className='obj-caption text-app-green text-xl mb-3 font-semibold'>Find</h4>
-                                <p className='obj-text'>
+                                <p className='obj-text dark:text-white'>
                                     Search for microjobs posted<br/> by individuals around the school
                                 </p>
                             </div>
